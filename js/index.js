486468f5d1e6b0ed4f56bbd91c14c3a127476029
JanaMohamed8 <jana123mohamed77@gmail.com>
2025-04-15 18:39:41 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+      rtl: true,
+      loop: true,
+      margin: 24,
+      nav: true,
+      mouseDrag: true,
+      touchDrag: true,
+      autoplay: true,      
+      autoplayTimeout: 2000,   
+      autoplayHoverPause: true,
+      items: 1
+    });
+  });
+  
+
+
+
+const questions = document.querySelectorAll("#questions-list li:not(.content-li)");
+
+questions.forEach((li) => {
+  li.addEventListener("click", () => {
+    li.classList.toggle("active");
+
+    questions.forEach((otherLi) => {
+      if (otherLi !== li) {
+        otherLi.classList.remove("active");
+      }
+    });
+  });
+});
+
+   
+window.onload = () => {
+    setTimeout(() => {
+        document.getElementById("loading-screen").style.display = "none";
+        document.getElementById("content").style.display = "block";
+    }, 4000);     
+};
